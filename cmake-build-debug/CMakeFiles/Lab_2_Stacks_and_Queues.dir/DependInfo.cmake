17df814ca0e43adf8a12828b61b787f4b7e989fb
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-02-11 17:36:17 -0500

    deletable commit

---
+++ cmake-build-debug/CMakeFiles/Lab_2_Stacks_and_Queues.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/joshz/OneDrive - University of Waterloo/1B/Algorithms and Data Structures/Labs/Lab_2/Lab_2_Stacks_and_Queues/CircularQueue.cpp" "/cygdrive/c/Users/joshz/OneDrive - University of Waterloo/1B/Algorithms and Data Structures/Labs/Lab_2/Lab_2_Stacks_and_Queues/cmake-build-debug/CMakeFiles/Lab_2_Stacks_and_Queues.dir/CircularQueue.cpp.o"
+  "/cygdrive/c/Users/joshz/OneDrive - University of Waterloo/1B/Algorithms and Data Structures/Labs/Lab_2/Lab_2_Stacks_and_Queues/DynamicStack.cpp" "/cygdrive/c/Users/joshz/OneDrive - University of Waterloo/1B/Algorithms and Data Structures/Labs/Lab_2/Lab_2_Stacks_and_Queues/cmake-build-debug/CMakeFiles/Lab_2_Stacks_and_Queues.dir/DynamicStack.cpp.o"
+  "/cygdrive/c/Users/joshz/OneDrive - University of Waterloo/1B/Algorithms and Data Structures/Labs/Lab_2/Lab_2_Stacks_and_Queues/main.cpp" "/cygdrive/c/Users/joshz/OneDrive - University of Waterloo/1B/Algorithms and Data Structures/Labs/Lab_2/Lab_2_Stacks_and_Queues/cmake-build-debug/CMakeFiles/Lab_2_Stacks_and_Queues.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Lab_2_Stacks_and_Queues.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_2_Stacks_and_Queues.dir/CircularQueue.cpp.o"
+  "CMakeFiles/Lab_2_Stacks_and_Queues.dir/DynamicStack.cpp.o"
+  "CMakeFiles/Lab_2_Stacks_and_Queues.dir/main.cpp.o"
+  "Lab_2_Stacks_and_Queues.pdb"
+  "Lab_2_Stacks_and_Queues.exe"
+  "libLab_2_Stacks_and_Queues.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_2_Stacks_and_Queues.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
