0af2f0c8dca1c19af177d64260e8f64831536f81
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-02-11 16:15:19 -0500

    Initial Commit

---
+++ DynamicStack.cpp
@@ -0,0 +1,40 @@
+#include "DynamicStack.hpp"
+
+typedef DynamicStack::StackItem StackItem;  // for simplicity
+const StackItem DynamicStack::EMPTY_STACK = -999;
+
+DynamicStack::DynamicStack()
+{
+}
+
+DynamicStack::DynamicStack(unsigned int capacity)
+{
+}
+
+DynamicStack::~DynamicStack()
+{
+}
+
+bool DynamicStack::empty() const
+{
+}
+
+int DynamicStack::size() const
+{
+}
+
+void DynamicStack::push(StackItem value)
+{
+}
+
+StackItem DynamicStack::pop()
+{
+}
+
+StackItem DynamicStack::peek() const
+{
+}
+
+void DynamicStack::print() const
+{
+}
