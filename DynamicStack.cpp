910c8d78792f3c43b566e67fa361a1d6bc0b6ec9
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-03-04 23:41:54 -0500

    Completed Default constructor and basic methods for dynamic stack
    dynamic stack was updated to include basic functions. first dynamic stack test passes

--- DynamicStack.cpp
+++ DynamicStack.cpp
@@ -5,26 +5,42 @@ const StackItem DynamicStack::EMPTY_STACK = -999;
 
 DynamicStack::DynamicStack()
 {
+    capacity_ = 16;
+    size_=0;
+    init_capacity_ =16;
+    capacity_ = 16;
 }
 
 DynamicStack::DynamicStack(unsigned int capacity)
 {
+    items_ = new StackItem[capacity];
+    size_ = 0;
+    init_capacity_ = capacity;
+    capacity_ = capacity;
+
 }
 
 DynamicStack::~DynamicStack()
 {
+    delete[] items_;
 }
 
 bool DynamicStack::empty() const
 {
+    return size_==0;
 }
 
 int DynamicStack::size() const
 {
+    return size_;
 }
 
 void DynamicStack::push(StackItem value)
 {
+    if(capacity_-size_>=1){
+        //todo Should pointers or arrays be used. should i just move all items in the stack?
+    }
+
 }
 
 StackItem DynamicStack::pop()
