9bbacdf3403bab2af734117b2ad921cf06ad3fb6
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-03-07 20:59:08 -0500

    made the code compile by fixing errors. fixed various syntax and formatting errors
    the code now compiles. however there is no output this could be for a variety of reasons. possibly something to do with memory. there could be more than on instance of this happening. TODO has been left in order to guide you. comment out tests and code until you find the issue and then fix it. good luck

--- DynamicStack.cpp
+++ DynamicStack.cpp
@@ -1,17 +1,17 @@
 #include "DynamicStack.hpp"
+#include <iostream>
 
 typedef DynamicStack::StackItem StackItem;  // for simplicity
 const StackItem DynamicStack::EMPTY_STACK = -999;
+using namespace std;
 
-DynamicStack::DynamicStack()
-{
+DynamicStack::DynamicStack() {
     capacity_ = 16;
-    size_=0;
-    init_capacity_ =16;
+    size_ = 0;
+    init_capacity_ = 16;
 }
 
-DynamicStack::DynamicStack(unsigned int capacity)
-{
+DynamicStack::DynamicStack(unsigned int capacity) {
     items_ = new StackItem[capacity];
     size_ = 0;
     init_capacity_ = capacity;
@@ -19,75 +19,66 @@ DynamicStack::DynamicStack(unsigned int capacity)
 
 }
 
-DynamicStack::~DynamicStack()
-{
+DynamicStack::~DynamicStack() {
     delete[] items_;
 }
 
-bool DynamicStack::empty() const
-{
-    if(size_==0)
+bool DynamicStack::empty() const {
+    if (size_ == 0)
         return true;
     return false;
 }
 
-int DynamicStack::size() const
-{
+int DynamicStack::size() const {
     return size_;
 }
 
-void DynamicStack::push(StackItem value)
-{
-   if(size_==capacity_)
-   {
-       capacity_*=2;
-       StackItem* temp= new StackItem(capacity_);
-       for(int i=0; i<size; i++)
-           temp[i]=items_[i];
-       delete[] items_;
-       temp=items_;
-       temp=NULL;
-       items_[size_]=value;
-       size_++;
-       
-   }
-    else
-    {
-        items_[size_]=value;
+void DynamicStack::push(StackItem value) {
+    if (size_ == capacity_) {
+        capacity_ *= 2;
+        StackItem *temp = new StackItem(capacity_);
+        for (int i = 0; i < size_; i++)
+            temp[i] = items_[i];
+        delete[] items_;
+        temp = items_;
+        temp = NULL;
+        items_[size_] = value;
+        size_++;
+
+    } else {
+        items_[size_] = value; //todo this line of code is making the program crash. check it out. it prevent output. its possible because the index size_ is outside the array
         size_++;
     }
-    
-    
-}
-StackItem DynamicStack::pop()
-{
-    if(size_==0)
-        return EMPTY_STACK;
-    
-    StackItem temp1=items_[size_];
-    StackItem* temp2= new StackItem(size_);
-    for (int i=0; i<size_-1; i++)
-        temp2[i]=items_[i];
-    delete[] items_;
-    temp2=items_;
-    temp2=NULL;
-    if(size_<capacity/4)
-        capacity/=2;
-    return temp1;
-    
+
+
 }
 
-StackItem DynamicStack::peek() const
-{
-    
-    if(size_==0)
+StackItem DynamicStack::pop() {
+    if (size_ == 0)
         return EMPTY_STACK;
-    
+
+    StackItem temp1 = items_[size_];
+    StackItem *temp2 = new StackItem(size_);
+    for (int i = 0; i < size_ - 1; i++)
+        temp2[i] = items_[i];
+    delete[] items_;
+    temp2 = items_;
+    temp2 = nullptr;
+    if (size_ < capacity_ / 4)
+        capacity_ /= 2;
+    return temp1;
+
+}
+
+StackItem DynamicStack::peek() const {
+
+    if (size_ == 0)
+        return EMPTY_STACK;
+
     return items_[size_];
 }
 
-void DynamicStack::print() const
-{
-    for(int i=0; i<size_; i++)
-        cout<<items_[i]<<"\t";
+void DynamicStack::print() const {
+    for (int i = 0; i < size_; i++)
+        cout << items_[i] << "\t";
 }
