a594e2779aa804f007c72865bdc57a99bd678e9c
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-03-12 00:59:51 -0400

    added the test cases to the main

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@ int main()
     bool stack_test1_passed = stack_test.test1();
     bool stack_test2_passed = stack_test.test2();
     bool stack_test3_passed = stack_test.test3();
+    bool stack_test4_passed = stack_test.test4();
+    bool stack_test5_passed = stack_test.test5();
+    bool stack_test6_passed = stack_test.test6();
 
     bool queue_test1_passed = queue_test.test1();
     bool queue_test2_passed = queue_test.test2();
@@ -26,6 +29,9 @@ int main()
     cout << "Test1: " << get_status_str(stack_test1_passed) << endl;
     cout << "Test2: " << get_status_str(stack_test2_passed) << endl;
     cout << "Test3: " << get_status_str(stack_test3_passed) << endl;
+    cout << "Test4: " << get_status_str(stack_test4_passed) << endl;
+    cout << "Test5: " << get_status_str(stack_test5_passed) << endl;
+    cout << "Test6: " << get_status_str(stack_test6_passed) << endl;
     cout << endl;
     cout << "---Circular Queue Tests---" << endl;
     cout << "Test1: " << get_status_str(queue_test1_passed) << endl;
